1204b4fe3c7267343b90307904ccf02143d07ac0
Austin-Joshua <austinjoshuamj@gmail.com>
2026-02-26 21:21:06 +0530

    Replace logo.png with institutional-dark-logo.png in layouts

--- frontend/src/layouts/StudentLayout.jsx
+++ frontend/src/layouts/StudentLayout.jsx
@@ -86,7 +86,7 @@ const StudentLayout = () => {
                         {sidebarOpen ? (
                             /* Wide logo when sidebar open */
                             <img
-                                src="/assets/images/logo.png"
+                                src="/assets/images/institutional-dark-logo.png"
                                 alt="RIT Rajalakshmi Institute of Technology"
                                 style={{ height: '50px', width: 'auto', objectFit: 'contain', maxWidth: '200px' }}
                             />
@@ -183,7 +183,7 @@ const StudentLayout = () => {
                         </button>
                         <Link to="/" style={{ display: 'flex', alignItems: 'center', height: '100%', padding: 0 }}>
                             <img
-                                src="/assets/images/logo.png"
+                                src="/assets/images/institutional-dark-logo.png"
                                 alt="RIT"
                                 style={{ height: '50px', width: 'auto', objectFit: 'contain' }}
                             />
