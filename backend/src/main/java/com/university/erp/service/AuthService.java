03d5a88f2c990e4de5f8c84777089b6fcce1dc7e
Austin-Joshua <austinjoshuamj@gmail.com>
2026-04-07 06:59:24 +0530

    fix(auth): clean up unused imports from old google auth

--- backend/src/main/java/com/university/erp/service/AuthService.java
+++ backend/src/main/java/com/university/erp/service/AuthService.java
@@ -19,14 +19,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import com.university.erp.dto.GoogleAuthRequest;
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.gson.GsonFactory;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseToken;
 import org.springframework.beans.factory.annotation.Value;
-import java.util.Collections;
 import java.util.Optional;
 
 @Service
